13c90df2e71f533d97ab8ccc9c8796cb2f9499cc
ÄTAGWE ROGER <atagweroger2@gmail.com>
2026-07-15 10:11:38 +0100

    created a demo tasks list

---
+++ tasktracker.py
@@ -0,0 +1,11 @@
+import datetime
+print(" == Task Tracker ==")
+tasks = [{"id": 0 , "name": "Play Game" , "description": "Beat Richy" , "status": "todo" , "createdAt": datetime.datetime.now() , "updatedAt": datetime.datetime.now()},
+         {"id": 1 , "name": "Go to church" , "description": "Pray to Good" , "status": "in-progress" , "createdAt": datetime.datetime.now() , "updatedAt": datetime.datetime.now()}
+         ]
+
+
+
+
+
+     
