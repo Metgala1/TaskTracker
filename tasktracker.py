a43cf064a9596b683fdf7e41bb11184ca78df232
ÄTAGWE ROGER <atagweroger2@gmail.com>
2026-07-15 10:13:01 +0100

    Created addTask function

--- tasktracker.py
+++ tasktracker.py
@@ -5,6 +5,24 @@ tasks = [{"id": 0 , "name": "Play Game" , "description": "Beat Richy" , "status"
          ]
 
 
+def addTask():
+        task_name = input("Enter Task Name: ")
+        description = input("Describe Task: ")
+        status = "todo"
+        createdAt = datetime.datetime.now()
+        updateAt = datetime.datetime.now()
+        id = 2
+
+        myTask = {}
+        myTask["id"] = id
+        myTask["name"] = task_name
+        myTask["description"] = description
+        myTask["status"] = status
+        myTask["createdAt"] = createdAt
+        myTask["updatedAt"] = updateAt
+        
+        tasks.append(myTask)
+        id += 1
 
 
 
