8feaac8bb1023085e9d2a53ca7dd4a8533780d88
ÄTAGWE ROGER <atagweroger2@gmail.com>
2026-07-15 11:24:44 +0100

    Added 3 modules json os and sys

--- tasktracker.py
+++ tasktracker.py
@@ -1,4 +1,10 @@
 import datetime
+import json
+import os
+import sys
+
+
+
 print(" == Task Tracker ==")
 tasks = [{"id": 0 , "name": "Play Game" , "description": "Beat Richy" , "status": "todo" , "createdAt": datetime.datetime.now() , "updatedAt": datetime.datetime.now()},
          {"id": 1 , "name": "Go to church" , "description": "Pray to Good" , "status": "in-progress" , "createdAt": datetime.datetime.now() , "updatedAt": datetime.datetime.now()}
