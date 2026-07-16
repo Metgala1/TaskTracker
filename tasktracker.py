4343c36754af9b28d48cbea953b48fa51eae19c3
ÄTAGWE ROGER <atagweroger2@gmail.com>
2026-07-16 05:39:39 +0100

    added several other functions and my logic for my program

--- tasktracker.py
+++ tasktracker.py
@@ -33,90 +33,166 @@ print(" == Task Tracker == ")
 
 
 def addTask():
-        task_name = input("Enter Task Name: ")
-        description = input("Describe Task: ")
-        status = "todo"
-        createdAt = datetime.datetime.now()
-        updateAt = datetime.datetime.now()
-        id = 2
+        if len(sys.argv) < 4:
+             print("Usage: python task_tracker.py add <Task Name> <Description>")
+             return
+        task_name = sys.argv[2]
+        description = sys.argv[3]
 
-        myTask = {}
-        myTask["id"] = id
-        myTask["name"] = task_name
-        myTask["description"] = description
-        myTask["status"] = status
-        myTask["createdAt"] = createdAt
-        myTask["updatedAt"] = updateAt
-        
+        myTask = {
+             "id": generateId(),
+             "name": task_name ,
+             "description": description,
+             "status": "todo",
+             "createdAt": datetime.datetime.now().isoformat(),
+             "updatedAt": datetime.datetime.now().isoformat()
+        }
+
         tasks.append(myTask)
-        id += 1
+        saveTasks()
+        print("Tasked added successfully!")
+             
+             
 
 def updateTask(id):
-     task_to_update = {}
+     task_to_update = None
+
      for task in tasks:
           if task["id"] == id:
                task_to_update = task
-     print("Updating task: ", task_to_update["id"])
-     print("Current task name: ", task_to_update["name"])
-     print("Current task description: ", task_to_update["description"])
+               break
+     if task_to_update is None:
+          print("Task is not found")
+          return
+     if len(sys.argv) < 5:
+          print("Usage: python task_tracker.py add <Task Name> <Description>")
+          return
+     task_to_update["name"] = sys.argv[3]
+     task_to_update["description"] = sys.argv[4]
+     task_to_update["updatedAt"] = datetime.datetime.now().isoformat()
 
-     new_name = input("Edit name: ")
-     new_description = input("Enter new description")
-     status = input("Update status: ")
-     updated_time = datetime.datetime.now()
-
-     task_to_update["name"] = new_name
-     task_to_update["description"] = new_description
-     task_to_update["status"] = status
-     task_to_update["updateAt"] = updated_time
+     saveTasks()
+     print("Task updated successfully!")
+          
 
 def deleteTask(id):
     for i, task in enumerate(tasks):
         if task["id"] == id:
             tasks.pop(i)
+            saveTasks()
             print(f"Task with ID {id} deleted!")
             return
+    
 
 def displayTask():
-     num = 1
-     for task in tasks:
-          print(f"{num}. Name: {task["name"]}, Id: {task["id"]}")
-          num += 1
-
-while True:
-    print()
-    print("Select 1 to add Task")
-    print("Select 2 to View Tasks")
-    print("Select 3 to Update Task")
-    print("Select 4 to Delete Task ")
-    print("Select 5 to Exit Programm")
-    print()
-
-    operation = int(input("Choose operation: "))
-    match operation:
-         case 1:
-              addTask()
-         case 2:
-              displayTask()
-         case 3:
-              print()
-              for task in tasks:
-                 print(f"TaskId: {task["id"]}, TaskName: {task["name"]}")
-              id = int(input("Select id of task to edit: " ))
-              updateTask(id)
-         case 4:
-              print()
-              for task in tasks:
-                 print(f"Task name: {task["name"]} , taskId: {task["id"]}")
-              
-              task_to_delete = int(input("Enter the id of task to delete: "))
-              deleteTask(task_to_delete)
-         case 5:
-              print("Exiting Program...")
-              break
-         case _:
-              print("Invalid Operation")
-
-
-
+     if len(tasks) == 0:
+          print("No tasks available.")
+          return
      
+     for task in tasks:
+          print("----------------------------")
+          print(f"ID: {task['id']}")
+          print(f"Name: {task['name']}")
+          print(f"Description: {task['description']}")
+          print(f"Status: {task['status']}")
+          print(f"Created: {task['createdAt']}")
+          print(f"Updated: {task['updatedAt']}")
+
+def markInProgress(id):
+    for task in tasks:
+        if task["id"] == id:
+            task["status"] = "in-progress"
+            task["updatedAt"] = datetime.datetime.now().isoformat()
+            saveTasks()
+            print("Task marked as in-progress.")
+            return
+
+    print("Task not found!")
+
+def markDone(id):
+    for task in tasks:
+        if task["id"] == id:
+            task["status"] = "done"
+            task["updatedAt"] = datetime.datetime.now().isoformat()
+            saveTasks()
+            print("Task marked as done.")
+            return
+
+    print("Task not found!")
+
+def doneTask():
+    if not any(task["status"] == "done" for task in tasks):
+        print("You have no task with the status 'Done'")
+        return
+    doneTasks = [task for task in tasks if task["status"] == "done"]
+    for task in doneTasks:
+        print(f"ID: {task['id']} , {task['name']}")
+
+def todoTask():
+     if not any(task["status"] == "todo" for task in tasks):
+          print("You have no task with status of 'Todo'")
+          return
+     todoTask = [task for task in tasks if task["status"] == "todo"]
+     for task in todoTask:
+          print(f"ID: {task["id"]} , {task["name"]}")
+
+def inProgressTask():
+     if not any(task["status"] for task in tasks):
+          print("You have no tasks with the status of 'In-progress'")
+          return
+     inProgressTask = [task for task in tasks if task["status"] == "in-progress"]
+     for task in inProgressTask:
+          print(f"ID: {task["id"]} , {task["name"]}")
+
+
+
+               
+
+if len(sys.argv) < 2:
+     print("""
+Task Tracker Commands
+
+python3 tasktracker.py add "Task Name" "Description"
+python3 tasktracker.py update 1 "New Name" "New Description"
+python3 tasktracker.py delete 1
+python3 tasktracker.py list
+python3 tasktracker.py mark-in-progress 1
+python3 tasktracker.py mark-done 1
+python3 tasktracker.py done
+python3 tasktracker.py todo
+python3 tasktracker.py in-progress
+
+""")
+     sys.exit()
+     
+commamd = sys.argv[1]
+
+try:
+     match commamd:
+          case "add":
+               addTask()
+          case "list":
+               displayTask()
+          case "update":
+               updateTask(int(sys.argv[2]))
+          case "delete":
+               deleteTask(int(sys.argv[2]))
+          case "mark-in-progress":
+               markInProgress(int(sys.argv[2]))
+          case "mark-done":
+               markDone(int(sys.argv[2]))
+          case "done":
+               doneTask()
+          case "todo":
+               todoTask()
+          case "in-progress":
+               inProgressTask()
+          case _:
+               print("Unkwon command.")
+except ValueError:
+     print("ID must be a number")
+except IndexError:
+     print("Missing command arguments.")
+          
+     
+          
